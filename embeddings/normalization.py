42691bedca8520c2be0768072a2816e6f1f38399
Te Dza Zang <125554845+DzaZangTe@users.noreply.github.com>
2024-10-26 18:09:39 +0800

    Update normalization.py

--- embeddings/normalization.py
+++ embeddings/normalization.py
@@ -16,15 +16,23 @@ def load_contractions(file_path):
 def normalize_words(text): # stupid but works
     words = []
     contractions = load_contractions("contractions.txt")
+    text = unidecode.unidecode(text) # e.g. "café" -> "cafe"
+    text = text.lower()
     for contraction, expansion in contractions.items():
         text = re.sub(rf"\b{contraction}\b", " ".join(expansion), text) # e.g. "I'm" -> "I am"
-    text = unidecode.unidecode(text) # e.g. "café" -> "cafe"
     text = re.sub(r"[`\[\]\"]", "", text) # e.g. "`rock[n]roll`" -> "rocknroll"
-    text = re.sub(r"(\w+)'s$", r"\1 's", text) # e.g. "John's" -> "John 's"
-    text = re.sub(r"(\w+s)'$", r"\1 's", text) # e.g. "dogs'" -> "dogs 's"
-    text = re.sub(r"[\']", "", text) # e.g. "rock'n'roll" -> "rocknroll"
+    text = re.sub(r"'s(?=[^a-zA-Z]|$)", r" 's ", text) # e.g. "John's" -> "John 's"
+    text = re.sub(r"s'(?=[^a-zA-Z]|$)", r"s 's ", text) # e.g. "dogs'" -> "dogs 's"
     text = re.sub(r"[/-]", " ", text) # e.g. "rock-n-roll" -> "rock n roll"
-    text = re.sub(r"([@#&$€£¥])", r" \1 ", text) # e.g. "rock&roll" -> "rock & roll"
+    text = re.sub(r"([@#&%+:,.?!$€£¥\(\)])", r" \1 ", text) # e.g. "rock&roll" -> "rock & roll"
+    text = re.sub(r"(?<!s)'(?!s\b\s|s$)", "", text) # e.g. "rock'n'roll" -> "rocknroll"
     for word in text.split():
         words.append(word)
     return words
+
+if __name__ == "__main__":
+    text = "Check the price of the new gadget ($199) at 'Tech-Store', " \
+           "and don't forget to use the discount code 'SAVE20' for 20% off on your next purchase! " \
+           "For more info, call #123 or visit www.tech-store.com & sign up."
+    print(text)
+    print(normalize_words(text))
